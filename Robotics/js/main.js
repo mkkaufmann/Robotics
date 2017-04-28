a8bb7f7ac038f96e3b5c3f9b5f8dba7061b3fba3
mkkaufmann <mkaufmann421@gmail.com>
2017-04-28 09:04:58 -0400

    Add files via upload

--- Robotics/js/main.js
+++ Robotics/js/main.js
@@ -2,12 +2,5 @@
 /*global $, jQuery, alert*/
 $(function () {
     "use strict";
-    $("#collapseOne").on("show.bs.collapse", function () {
-        $("#2017DropIcon").removeClass("fa-angle-down");
-        $("#2017DropIcon").addClass("fa-angle-up");
-    });
-    $("#collapseOne").on("hide.bs.collapse", function () {
-        $("#2017DropIcon").removeClass("fa-angle-up");
-        $("#2017DropIcon").addClass("fa-angle-down");
-    });
+    $("#nav-container").css("width", screen.width);
 });
