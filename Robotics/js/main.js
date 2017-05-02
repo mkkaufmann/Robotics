b6924c11d97f39e111ea3bde64e1dbb9ec571b36
mkkaufmann <mkaufmann421@gmail.com>
2017-05-02 10:32:18 -0400

    Add files via upload

--- Robotics/js/main.js
+++ Robotics/js/main.js
@@ -2,5 +2,35 @@
 /*global $, jQuery, alert*/
 $(function () {
     "use strict";
-    $("#nav-container").css("width", screen.width);
+    var safeAddClass = function (item, className) {
+        if (!item.hasClass(className)) {
+            item.addClass(className);
+        }
+    };
+    var safeRemoveClass = function (item, className) {
+        if (item.hasClass(className)) {
+            item.removeClass(className);
+        }
+    };
+    var checkSize = function () {
+        if ($(window).width() >= 992) {
+            safeRemoveClass($("#nav-container"), "mdbreak");
+            safeAddClass($("#nav-container"), "lgbreak");
+            safeAddClass($("#dropdown"), "dropdownlgbreak");
+            safeAddClass($("#season2016"), "season2016lgbreak");
+            safeAddClass($("#ourteam"), "ourteamlgbreak");
+            safeAddClass($("#contactus"), "contactuslgbreak");
+            safeAddClass($("#sponsorus"), "sponsoruslgbreak");
+        } else {
+            safeRemoveClass($("#nav-container"), "lgbreak");
+            safeAddClass($("#nav-container"), "mdbreak");
+            safeRemoveClass($("#dropdown"), "dropdownlgbreak");
+            safeRemoveClass($("#season2016"), "season2016lgbreak");
+            safeRemoveClass($("#ourteam"), "ourteamlgbreak");
+            safeRemoveClass($("#contactus"), "contactuslgbreak");
+            safeRemoveClass($("#sponsorus"), "sponsoruslgbreak");
+        }
+    };
+    checkSize();
+    $(window).resize(checkSize);
 });
